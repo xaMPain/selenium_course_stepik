f380df7d969263d4b8b50cfb146c3aff9a4dcf27
xaMPain <xampain666@gmail.com>
2019-10-08 18:29:51 +0300

    add base and main page (Page Object)

---
+++ base_page.py
@@ -0,0 +1,9 @@
+
+# создаем класс
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
