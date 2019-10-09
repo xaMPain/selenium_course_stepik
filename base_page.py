8132a3fbd168e388dd83562118613e4f834b43f7
xaMPain <xampain666@gmail.com>
2019-10-09 13:10:48 +0300

    add element assertion

--- base_page.py
+++
@@ -1,9 +0,0 @@
-
-# создаем класс
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-
-    def open(self):
-        self.browser.get(self.url)
