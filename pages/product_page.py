122aa565b7d4590ffb2d2b5b10ef0d1e47822888
xaMPain <xampain666@gmail.com>
2019-10-11 14:31:37 +0300

    upgrade script for search product name and price

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,26 +11,23 @@ class ProductPage(BasePage):
         add_button = self.browser.find_element(*ProductPageLocators.PRODUCT_ADD_BUTTON)
         add_button.click()
 
-    def should_be_success_message(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product is same"
-        assert True
-
     def should_item_in_basket(self):
         self.should_be_same_product()
         self.should_be_same_price()
 
-
     def should_be_same_product(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product is wrong"
-        assert True
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Name of product not found"
+        self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
     def should_be_same_price(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Price is wrong"
-        assert True
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Price of product not found"
+        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 
     def should_be_success_message(self):
         msg_lst = self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGES)
         assert len(msg_lst) == 3, "Success message not found"
+        assert self.product_name == msg_lst[0].text, "Wrong name product added to basket"
+        assert self.product_price == msg_lst[2].text, "Wrong price product added to basket"
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -2,13 +2,15 @@ from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.should_be_add_to_cart_button()  # проверяем что есть кнопка добавления в корзину
     page.add_product_to_cart()  # жмем кнопку добавить в корзину
     page.solve_quiz_and_get_code()  # решаем задание и вносим ответ
-    page.should_item_in_basket()  # проверяем товар в корзине
+    page.should_be_same_product()  # проверяем и запоминаем имя продукта
+    page.should_be_same_price()  # проверям и запоминаем цену продукта
     page.should_be_success_message()  # проверяем что есть сообщение с нужным текстом
+    page.should_item_in_basket()  # проверяем товар в корзине
 
 
